2a6e252a02a1de73495dab60f38d5b316ae894ea
Giovanni Vinaccia <vinaccia.giovanni@gmail.com>
2018-05-08 19:18:20 +0200

    wip

---
+++ desktop/src/api/Client.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export class Client {
+
+  constructor() {
+
+  }
+
+  refreshToken() {
+
+  }
+
+}
--- desktop/src/index.ts
+++ desktop/src/index.ts
@@ -1,11 +1,32 @@
 import { app, BrowserWindow } from 'electron';
-import { join } from 'path';
+import { join, resolve } from 'path';
 import { format } from 'url';
+import * as fs from 'graceful-fs';
 
 let win: BrowserWindow;
 
+const configPath = app.getPath('userData');
+
+const secretPath = join(configPath, '.token');
+
+if (!fs.existsSync(secretPath)) {
+  process.exit(1);
+}
+
+const secret = fs.readFileSync(secretPath, 'utf-8');
+
+console.log(secret);
+
 app.on('ready', () => {
-  win = new BrowserWindow();
+
+  win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: false,
+      preload: resolve(join(__dirname, 'preload.js'))
+    }
+  });
+
+  win.on('show', () => win.webContents.toggleDevTools());
 
   win.loadURL(format({
     protocol: 'file',
