6ed1f6e64c626ca51865ca3f3e7a8a706b61198a
Giovanni Vinaccia <vinaccia.giovanni@gmail.com>
2018-06-07 20:33:47 +0200

    improves types

--- shared/src/storage-item.ts
+++ shared/src/storage-item.ts
@@ -28,6 +28,31 @@ export interface StorageItem {
   parentId: StorageItemId;
 }
 
+export interface RawContentData {
+  extraMetadata: { };
+  fileType: ''|'pdf';
+  fontName: string;
+  lastOpenedPage: number;
+  lineHeight: number;
+  margins: number;
+  pageCount: number;
+  textScale: number;
+
+  // transformation matrix
+  transform: {
+    m11: 0|1;
+    m12: 0|1;
+    m13: 0|1;
+    m21: 0|1;
+    m22: 0|1;
+    m23: 0|1;
+    m31: 0|1;
+    m32: 0|1;
+    m33: 0|1;
+  };
+}
+
 export interface Notebook extends StorageItem {
   pages?: { path: string; }[];
+  content?: RawContentData;
 }
